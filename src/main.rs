77435dca5f22c25e49121e9b6a4a66d3b3afafd6
Akhil Bhandaru <bhandaru.akhil@gmail.com>
2020-11-26 01:19:11 -0500

    Started working on CLI, using structopt and paw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path;
+use structopt::StructOpt;
+
+struct Cli {
+   query: String,
+   filename: path::PathBuf,
 }
+
+fn main() {
+    let query: String = env::args().nth(1).expect("No query given");
+    let filename: String = env::args().nth(2).expect("No csv filename given");
+    let args = Cli {
+        query: query,
+        filename: path::PathBuf::from(filename),
+    };
+
+    println!("Searching for {}", args.query);
+    println!("In file {}", args.filename.into_os_string().into_string().unwrap());
+}
+
